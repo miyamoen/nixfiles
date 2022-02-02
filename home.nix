a4b4210ff8de11627dd57a0daa48192d47841bdb
miyamoen <akihito.biblioteca@gmail.com>
2022-02-03 01:44:28 +0900

    getting started with Home Manager

---
+++ home.nix
@@ -0,0 +1,30 @@
+{ config, pkgs, ... }:
+
+{
+  # Home Manager needs a bit of information about you and the
+  # paths it should manage.
+  home.username = "miyamo";
+  home.homeDirectory = "/home/miyamo";
+
+  # This value determines the Home Manager release that your
+  # configuration is compatible with. This helps avoid breakage
+  # when a new Home Manager release introduces backwards
+  # incompatible changes.
+  #
+  # You can update Home Manager without changing this value. See
+  # the Home Manager release notes for a list of state version
+  # changes in each release.
+  home.stateVersion = "22.05";
+
+  # Let Home Manager install and manage itself.
+  programs.home-manager.enable = true;
+
+  programs.git = {
+    enable = true;
+    userName = "miyamoen";
+    userEmail = "akihito.biblioteca@gmail.com";
+    aliases = {
+      st = "status";
+    };
+  };
+}
